1a4c1985ff9faf0494ec6537cbd429e69fc2a713
CC107@ccs.local <CC107@ccs.local>
2019-08-04 05:06:32 +0800

    done

--- application/models/News_model.php
+++ application/models/News_model.php
@@ -5,8 +5,7 @@ class News_model extends CI_Model {
         {
                 $this->load->database();
         }
-}
-public function get_news($slug = FALSE)
+        public function get_news($slug = FALSE)
 {
         if ($slug === FALSE)
         {
@@ -17,3 +16,20 @@ public function get_news($slug = FALSE)
         $query = $this->db->get_where('news', array('slug' => $slug));
         return $query->row_array();
 }
+public function set_news()
+{
+    $this->load->helper('url');
+
+    $slug = url_title($this->input->post('title'), 'dash', TRUE);
+
+    $data = array(
+        'title' => $this->input->post('title'),
+        'slug' => $slug,
+        'text' => $this->input->post('text')
+    );
+
+    return $this->db->insert('news', $data);
+}
+
+}
+
